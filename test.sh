4167648a174eb6c07cbbaef949a20ce4c125ce87
santoshdheke <santoshdheke46@gmail.com>
2022-05-03 11:43:20 +0545

    test
    asdf

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
-read name
+echo "enter sqlfile name";
+read sqlfile;
+echo "user database user";
+read user;
 
-echo $name;
+echo "database";
+read database;
+
+mysql -u $user -p $database < $(pwd)/$sqlfile
+
